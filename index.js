2d8968588ae6e3b34df69185d282419fab1bf811
Abhishek12589 <hindujaabhishek5@gmail.com>
2024-04-03 19:49:01 +0530

    Testing the api with postman

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require("express")
 const app = express()
-const readLineSync = require("readline-sync")
 
-var name = readLineSync.question("What is your Name ")
-console.log("Hello " + name)
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+var ingredients = [
+    {
+        "id":"8596AK",
+        "text":"bacon"
+    },
+    {
+        "id":"7856AK",
+        "text":"Cheese"
+    },
+    {
+        "id":"85996AK",
+        "text":"Roast"
+    },
+    {
+        "id":"7396AK",
+        "text":"Milk"
+    },
+
+]
+app.get("/", function(req,res){
+    res.send(ingredients)
+})
 
 var port = 4000
 
+
+app.post("/", function(req,res){
+    var ingredient = req.body
+    if (!ingredient || ingredient.text === ""){
+        res.status(200).send({error:"Your Ingredient Must have Text"})
+    }
+    else{
+        ingredients.push(ingredient)
+        res.status(200).send(ingredients)
+    }
+})
 
 
 app.listen(port,(req,res)=>{
